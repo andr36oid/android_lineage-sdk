b5dd9af871220714332a79a310a53e30b423e549
sonic011gamer <efimgirjeu@gmail.com>
2025-07-27 08:39:26 +0300

    Remove the trust notifications since they never want to go away on their own. (plus we're on a console so we don't wanna see that)

--- lineage/lib/main/java/org/lineageos/platform/internal/TrustInterfaceService.java
+++ lineage/lib/main/java/org/lineageos/platform/internal/TrustInterfaceService.java
@@ -102,7 +102,7 @@ public class TrustInterfaceService extends LineageSystemService {
 
             // Onboard
             if (!hasOnboardedUser()) {
-                postOnBoardingNotification();
+                //postOnBoardingNotification();
                 registerLocaleChangedReceiver();
                 return;
             }
@@ -207,12 +207,12 @@ public class TrustInterfaceService extends LineageSystemService {
     private void runTestInternal() {
         int selinuxStatus = getSELinuxStatus();
         if (selinuxStatus != TrustInterface.TRUST_FEATURE_LEVEL_GOOD) {
-            postNotificationForFeatureInternal(TrustInterface.TRUST_WARN_SELINUX);
+            //postNotificationForFeatureInternal(TrustInterface.TRUST_WARN_SELINUX);
         }
 
         int keysStatus = getKeysStatus();
         if (keysStatus != TrustInterface.TRUST_FEATURE_LEVEL_GOOD) {
-            postNotificationForFeatureInternal(TrustInterface.TRUST_WARN_PUBLIC_KEY);
+            //postNotificationForFeatureInternal(TrustInterface.TRUST_WARN_PUBLIC_KEY);
         }
     }
 
@@ -358,7 +358,7 @@ public class TrustInterfaceService extends LineageSystemService {
             if (intent.getAction() == Intent.ACTION_LOCALE_CHANGED) {
                 if (!hasOnboardedUser()) {
                     // When are not onboarded, we want to change the language of the notification
-                    postOnBoardingNotification();
+                    //postOnBoardingNotification();
                 } else {
                     // We don't care anymore about language changes
                     context.unregisterReceiver(this);
